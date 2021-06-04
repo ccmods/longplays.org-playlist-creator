a33a942194c90c28f1a90534fbc381dfa9810ec8
ccmods <ccmods@gmail.com>
2021-06-03 20:34:35 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -20,29 +20,35 @@ namespace retrolongplay
             int cat = -1;
             string tarPlist = args[1];
             if(!int.TryParse(args[0],out cat) || string.IsNullOrEmpty(tarPlist.Trim())) {return;}
-            string games = getGamesByCategory(cat);
-            
-            Videos allVideos = JsonConvert.DeserializeObject<Videos>(games); 
-            HtmlDocument doc = new HtmlDocument();
-            StringBuilder sb = new StringBuilder();
-            foreach(AaData gameData in allVideos.aaData) {                
-                doc.LoadHtml(gameData.game);
-                HtmlNodeCollection gameNodes = doc.DocumentNode.ChildNodes;
-                string gameName = gameNodes[0].InnerText;
-                string gameId = gameNodes[0].GetAttributeValue("href","");
-                if(gameId.Contains("="))
-                    gameId = gameId.Split(new string[] {Constants.Tokens.longPlayId}, StringSplitOptions.None)[1].Substring(1);
-                doc.LoadHtml(gameData.players);
-                HtmlNodeCollection playerNodes = doc.DocumentNode.ChildNodes;    
-                string player = playerNodes[1].InnerText;
-                sb.AppendLine(
-                    string.Format(Constants.trackItem,
-                        Constants.Endpoints.videoUrl+gameId,
-                        SecurityElement.Escape(HttpUtility.HtmlDecode(gameName.Trim())),player.Trim())
-                );
-            }
-            File.WriteAllText(tarPlist,
-                Constants.playlistTemplate.Replace(Constants.Tokens.playlistToken, sb.ToString()));
+            try {
+                Console.WriteLine("Parameters correct. Retrieving video list.");
+                string games = getGamesByCategory(cat);
+                Console.WriteLine("Games retrieved. Formatting playlist.");
+                Videos allVideos = JsonConvert.DeserializeObject<Videos>(games); 
+                HtmlDocument doc = new HtmlDocument();
+                StringBuilder sb = new StringBuilder();
+                foreach(AaData gameData in allVideos.aaData) {                
+                    doc.LoadHtml(gameData.game);
+                    HtmlNodeCollection gameNodes = doc.DocumentNode.ChildNodes;
+                    string gameName = gameNodes[0].InnerText;
+                    string gameId = gameNodes[0].GetAttributeValue("href","");
+                    if(gameId.Contains("="))
+                        gameId = gameId.Split(new string[] {Constants.Tokens.longPlayId}, StringSplitOptions.None)[1].Substring(1);
+                    doc.LoadHtml(gameData.players);
+                    HtmlNodeCollection playerNodes = doc.DocumentNode.ChildNodes;    
+                    string player = playerNodes[1].InnerText;
+                    sb.AppendLine(
+                        string.Format(Constants.trackItem,
+                            Constants.Endpoints.videoUrl+gameId,
+                            SecurityElement.Escape(HttpUtility.HtmlDecode(gameName.Trim())),player.Trim())
+                    );
+                }
+                File.WriteAllText(tarPlist,
+                    Constants.playlistTemplate.Replace(Constants.Tokens.playlistToken, sb.ToString()));
+            } catch (Exception ex) {
+                Console.WriteLine("Error generating playlist - details:"+ex.ToString());
+            }   
+            Console.WriteLine("Process halted.");         
         }
 
         static string getGamesByCategory(int catId) {
